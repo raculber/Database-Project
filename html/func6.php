0725d9e4be7db07abcde16def608cfd6453ec29a
mvpanis <60119225+mvpanis@users.noreply.github.com>
2020-04-13 14:45:49 -0500

    updated last 3 functions

--- html/func6.php
+++ html/func6.php
@@ -1,17 +1,18 @@
 <html>
 <!--View all courses for a given student (given the StudentID, display all attributes in the Course table for each course)--> 
 <body>
-
-
-
+        <h3>Option 6 selected</h3><br>
+        <form action="func4.html" method="post">
+        <p>Enter Student ID below to view all courses for specific student</p>
+        Student ID: <input type="text" name="studentid" id="studentid"><br>
         <button type="button" onclick=main()>Main Menu</button>
         <button type="button" onclick=main()>Other Menu</button>
         <p id="error"></p>
 </body>
-
-<script>
-
-
-</script>
-
 </html>
+<?php
+if (isset($_POST['submit']))
+{
+   echo ""  
+}
+?>
