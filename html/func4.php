888456542e6686a53a80ca64c592bc78e8a95345
mvpanis <60119225+mvpanis@users.noreply.github.com>
2020-04-13 18:55:48 -0500

    updated functions

--- html/func4.php
+++ html/func4.php
@@ -2,14 +2,10 @@
 <!--View all students (display all attributes in the table for each student)--> 
 <body>
     <h3>Option 4 selected</h3><br>
-    <form action="func4.html" method="post">
+    <form action="func4.php" method="post">
     <p>Click button below to view all students</p>
     <input name="submit" type="submit" value="View Students">
     </form>
-
-        <button type="button" onclick=main()>Main Menu</button>
-        <button type="button" onclick=main()>Other Menu</button>
-        <p id="error"></p>
 </body>
 <?php
 include('php_db.php');
--- html/func5.php
+++ html/func5.php
@@ -2,15 +2,11 @@
 <!--View all courses from a given department (display all attributes in the Course table for each course)--> 
 <body>
         <h3>Option 5 selected/h3><br>
-        <form action="func4.html" method="post">
+        <form action="func5.php" method="post">
         <p>Enter department code below to view all courses in specific department</p>
         Department code: <input type="text" name="code" id="code"><br>
-        <input name="submit" type="submit" value="View all courses">
+        <input name="submit" type="submit" value="View courses for department">
         </form>
-
-        <button type="button" onclick=main()>Main Menu</button>
-        <button type="button" onclick=main()>Other Menu</button>
-        <p id="error"></p>
 </body>
 </html>
 <?php
@@ -18,6 +14,9 @@ include('php_db.php');
 
 if (isset($_POST['submit']))
 {
-   echo ""  
+    echo '<br>Courses from given department<br>';
+    //SELECT all from given department
+    $DeptCode = $myDb->query("SELECT * FROM Student");
+    $myDb->printTable($Student);  
 }
 ?>
