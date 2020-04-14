2643f1da985f1ad4d69e4c4575498a39d6ebcc15
mvpanis <60119225+mvpanis@users.noreply.github.com>
2020-04-14 17:23:34 -0500

    func5

--- html/func5.php
+++ html/func5.php
@@ -1,7 +1,7 @@
 <html>
 <!--View all courses from a given department (display all attributes in the Course table for each course)--> 
 <body>
-        <h3>Option 5 selected/h3><br>
+        <h3>Option 5 selected</h3><br>
         <form action="func5.php" method="post">
         <p>Enter department code below to view all courses in specific department</p>
         Department code: <input type="text" name="DeptCode" id="DeptCode"><br>
