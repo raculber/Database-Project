c144cb7b6c36d80274b3aa14b6d8e61d9cbb7cd1
raculber <rculbert5678@gmail>
2020-04-16 23:05:34 -0500

    removed primary key duplicates, added more inserts

--- project.sql
+++ project.sql
@@ -8,13 +8,14 @@ CREATE TABLE Course (
 	DeptCode CHAR(50) NOT NULL DEFAULT '',
 	CourseNum INT NOT NULL DEFAULT 0,
 	Title CHAR(50) NOT NULL DEFAULT '',
-	CreditHours INT NOT NULL DEFAULT 0
+	CreditHours INT NOT NULL DEFAULT 0,
+	PRIMARY KEY (DeptCode, CourseNum)
 );
 CREATE TABLE Enrollment (
 	StudentId INT,
 	DeptCode CHAR(50) NOT NULL DEFAULT '',
 	CourseNum INT NOT NULL DEFAULT 0,
-	PRIMARY KEY (StudentId)
+	PRIMARY KEY (StudentId, DeptCode, CourseNum)
 );
 
 INSERT INTO Student (StudentId, StudentName, Major)
@@ -23,6 +24,8 @@ INSERT INTO Student (StudentId, StudentName, Major)
 VALUES (019283456, 'John Doe', 'Business');
 INSERT INTO Student (StudentId, StudentName, Major)
 VALUES (039245673, 'Jane Smith', 'Communication');
+INSERT INTO Student (StudentId, StudentName, Major)
+VALUES (209301939, 'Tyler Saitang', "Computer Science");
 
 INSERT INTO Course (DeptCode, CourseNum, Title, CreditHours)
 VALUES ('CSCE', 4013, 'Cloud Computing', 3);
@@ -30,10 +33,14 @@ INSERT INTO Course (DeptCode, CourseNum, Title, CreditHours)
 VALUES ('PHYS', 2054, 'University Physics I', 4);
 INSERT INTO Course (DeptCode, CourseNum, Title, CreditHours)
 VALUES ('COMM', 1313, 'Public Speaking', 3);
+INSERT INTO Course (DeptCode, CourseNum, Title, CreditHours)
+VALUES ('CSCE', 2014, 'Programming Foundations II', 4);
 
 INSERT INTO Enrollment (StudentId, DeptCode, CourseNum)
 VALUES (019283456, 'PHYS', 2054);
 INSERT INTO Enrollment (StudentId, DeptCode, CourseNum)
 VALUES (010854980, 'CSCE', 4013);
+INSERT INTO Enrollment (StudentId, DeptCode, CourseNum)
+VALUES (010854980, 'COMM', 1313);
 INSERT INTO Enrollment (StudentId, DeptCode, CourseNum)
 VALUES (039245673, 'COMM', 1313);
